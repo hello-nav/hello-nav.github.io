62a1a9e016aa1ea5c4e69c851158594f94c2b7bf
Heroor <2388697970@qq.com>
2021-06-11 13:13:45 +0000

    CI BUILD

---
+++ precache-manifest.8d0bdbb02293ef6f071bc625e72be169.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04b0813d06c25af5f6df54f9a936bf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a98fd9579b5ab7e885",
+    "url": "/static/js/2.f0d0b683.chunk.js"
+  },
+  {
+    "revision": "1b7ccd5f7e533fd38779",
+    "url": "/static/js/main.fdec16fc.chunk.js"
+  },
+  {
+    "revision": "7afb10e8d62a811a23cb",
+    "url": "/static/js/runtime~main.46b21fbf.js"
+  },
+  {
+    "revision": "3f2d405be1bc8ee5acadd7fdf330ce55",
+    "url": "/static/media/angular.3f2d405b.svg"
+  },
+  {
+    "revision": "d63d573fd54e88d8ea20dae571f1bdde",
+    "url": "/static/media/goober.d63d573f.png"
+  },
+  {
+    "revision": "d41014a43be2ab589e539d41d93e406b",
+    "url": "/static/media/jenkins.d41014a4.png"
+  },
+  {
+    "revision": "89be4a45b064c97f8034e9e612caae8f",
+    "url": "/static/media/lerna.89be4a45.png"
+  },
+  {
+    "revision": "2d8ee815146390d567706f2c7b5c2916",
+    "url": "/static/media/nyantocat.2d8ee815.gif"
+  },
+  {
+    "revision": "b48b281ea9e16c6da6e5a9e9eb586d4f",
+    "url": "/static/media/tinypng.b48b281e.png"
+  }
+]);
